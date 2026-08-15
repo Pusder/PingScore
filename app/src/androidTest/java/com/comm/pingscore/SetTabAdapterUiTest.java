f5eb675e7bb2af712a15ae335c49ff2cab08b295
Pusder <pusder@qq.com>
2026-08-15 20:17:06 +0800

    fix(ui): restore portrait settings icon

--- app/src/androidTest/java/com/comm/pingscore/SetTabAdapterUiTest.java
+++ app/src/androidTest/java/com/comm/pingscore/SetTabAdapterUiTest.java
@@ -16,20 +16,24 @@ import java.util.Arrays;
 @RunWith(AndroidJUnit4.class)
 public final class SetTabAdapterUiTest {
     @Test
-    public void bindRendersCurrentAndPendingSetLabels() {
+    public void bindRendersCompletedCurrentAndPendingSetLabels() {
         Context context = ApplicationProvider.getApplicationContext();
         SetTabAdapter adapter = new SetTabAdapter(Arrays.asList(
+                new SetTabAdapter.Item(1, SetTabAdapter.COMPLETED, "11:8", true),
                 new SetTabAdapter.Item(2, SetTabAdapter.CURRENT, "0:0", true),
                 new SetTabAdapter.Item(3, SetTabAdapter.PENDING, "待赛", false)),
                 position -> { });
         FrameLayout parent = new FrameLayout(context);
 
+        SetTabAdapter.TabViewHolder completedHolder = adapter.onCreateViewHolder(parent, 0);
         SetTabAdapter.TabViewHolder currentHolder = adapter.onCreateViewHolder(parent, 0);
         SetTabAdapter.TabViewHolder pendingHolder = adapter.onCreateViewHolder(parent, 0);
 
-        adapter.onBindViewHolder(currentHolder, 0);
-        adapter.onBindViewHolder(pendingHolder, 1);
+        adapter.onBindViewHolder(completedHolder, 0);
+        adapter.onBindViewHolder(currentHolder, 1);
+        adapter.onBindViewHolder(pendingHolder, 2);
 
+        assertEquals("第1局\n11:8", completedHolder.label.getText().toString());
         assertEquals("第2局\n0:0", currentHolder.label.getText().toString());
         assertEquals("第3局\n待赛", pendingHolder.label.getText().toString());
     }
